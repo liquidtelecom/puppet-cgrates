5437768ae0b6d6bbb05638d2004fa363d0bc99d6
Anthony Somerset <anthony@somersettechsolutions.co.uk>
2019-05-14 15:41:40 +0200

    basic config file generator

---
+++ manifests/config_generator.pp
@@ -0,0 +1,52 @@
+# == DEFINE: cgrates::config_generator
+#
+#  This will define and deploy a single config file from an inbound HASH
+#
+# === Requirements
+#
+#  The following is needed :
+#   - puppetlabs-apt
+#
+# === Parameters
+#
+# WIP
+#
+# === Authors
+#
+# Author Name: Anthony Somerset
+#
+# === Copyright
+#
+# Copyright 2019 Liquid Telecom
+#
+
+# File needs a name e.g. general.json
+# file will contain a single section e.g. "general"
+# a section will ultimately be contained as a JSON object
+# potentially have some kind of precedence prefixed to file name 
+# e.g. a_general.json, b_datadb.json etc
+include cgrates::params
+define cgrates::config_generator (
+	$config_section	= "$name",
+	$precedence 	= undef,
+	$config_hash	= undef,
+	$config_path	= $cgrates::params::config_path,
+	$ensure			= 'present',
+) {
+
+	if ( $precedence ) {
+		$filename = "$precendence_$name"
+	} else {
+		$filename = $name
+	}
+
+	file { "${config_path}/${filename}.json":
+  		ensure 		=> $ensure,
+  	  	owner  		=> 'root',
+  	  	group  		=> 'root',
+  	  	mode   		=> '0644',
+	    content 	=> template('cgrates/config_file.erb'),
+		notify		=> Service["cgrates"],
+	}
+
+}
